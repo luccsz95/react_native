0d151e0ae84ce848715ac483982265ea97e9fcb8
Marion101200 <124160461+Marion101200@users.noreply.github.com>
2025-02-03 15:02:37 +0100

    changement de style pour le login

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Inscription from './composant/inscription';
+import { Login } from './composant/login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Inscription/>
+      <Login/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ composant/login.js
@@ -0,0 +1,79 @@
+﻿import React from "react";
+import {View, Text, StyleSheet, TextInput, Button} from "react-native";
+import { useState, useEffect } from 'react';
+
+
+export function Login(){
+    const [email, setEmail]= useState('');
+    const [password, setPassword]= useState('');
+    const [error, setError]= useState('');
+    
+    const VerifLogin = () => {
+        if(!email || !password) {
+           setError('Veuillez remplir tous les champs');
+        }
+    }
+    
+    useEffect(() => {
+        setError('');
+    }, [email, password]
+    );
+    
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Connexion</Text>
+            
+            <TextInput
+                style={styles.input}
+                placeholder="Email"
+                onChangeText={text=>setEmail(text)}
+            />
+            
+            <TextInput
+                style={styles.input}
+                placeholder="Mot de passe"
+                onChangeText={text=>setPassword(text)}
+            />
+
+            {error ? <Text style={styles.errormessages}>{error}</Text> : null}
+            
+            <Button 
+                style={styles.button} 
+                color={'#00bfff'}
+                title="Se connecter" onPress={VerifLogin}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 20,
+    },
+    
+    input: {
+        height: 50,
+        borderWidth: 1,
+        margin: 12,
+        padding: 10,
+        width: 300,
+    },
+    
+    title: {
+        fontSize: 20,
+        textAlign: 'center',
+        marginBottom: 20,
+        color: '#00bfff\n',
+    },
+
+    errormessages : {
+        color: '#ec0000',
+        margin: 10,
+    },
+    
+    button: {
+        margin: 10,
+    }
+});
